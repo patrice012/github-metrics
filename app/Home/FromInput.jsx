07f58a61e0fa26b32d50bd61f165274b4b9f5417
patrice <patricedev02@gmail.com>
2023-11-21 15:11:15 +0000

    add Function to fetch data

--- app/Home/FromInput.jsx
+++ app/Home/FromInput.jsx
@@ -1,21 +1,53 @@
 "use client";
 
+import { useState } from "react";
+import { baseUrl } from "../api-endpoint";
+
 export const FormInput = () => {
+    const [user, setUser] = useState(null);
+    const [input, setInput] = useState(undefined);
+    const [loading, setLoading] = useState(false)
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const data = new FormData(e.target)
-        console.log(data)
+        setLoading(true);
+        (async () => {
+            try {
+                const res = await getUser(input);
+                setUser(res);
+            } catch (error) {
+                // handle error
+                console.error(error);
+            } finally {
+                setLoading(false)
+            }
+        })();
+    };
+
+    const handleChange = (inputValue) => {
+        setInput(inputValue);
     };
 
     return (
         <form onSubmit={handleSubmit} className="search--container">
             <input
+                onChange={(e) => handleChange(e.target.value)}
                 type="text"
                 placeholder="Let's look at someone metrics...Type the name here."
                 className="text--input"
             />
-            <button className="btn submit-btn">Search</button>
+            <button disabled={loading} className="btn submit-btn">
+                Submit
+            </button>
         </form>
     );
 };
+
+async function getUser(username) {
+    const url = `${baseUrl}/users/${username}`;
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error("Failed to fetch data.");
+    }
+    return res.json();
+}
--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { HomeInterface } from "./Home/homePage"
+import { HomeInterface } from "./Home/HomePage"
 
 
 export default function Home() {
