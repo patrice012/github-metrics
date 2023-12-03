a1cc2c7ff7ccb71e2f1cea25da170a3f383ef7e8
patrice <patricedev02@gmail.com>
2023-12-03 08:43:39 +0000

    update: refactoring code

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,6 +1,13 @@
+const JWT = process.env.API_KEY;
+
 export async function fetchData(url) {
     try {
-        const res = await fetch(url);
+        const res = await fetch(url, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${JWT}`,
+            },
+        });
         if (!res.ok) {
             throw new Error("Failed to fetch data.");
         }
