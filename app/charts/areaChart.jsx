6e417cc47fd4191916a5ae5e4caf223f41bb47ce
patrice <patricedev02@gmail.com>
2023-12-04 12:54:29 +0000

    update area chart

--- app/charts/areaChart.jsx
+++ app/charts/areaChart.jsx
@@ -16,36 +16,47 @@ const AreaChartPlot = () => {
             year: "2016",
             Iphone: 4000,
             Samsung: 2400,
+            pixel: 7000,
         },
         {
             year: "2017",
             Iphone: 3000,
             Samsung: 1398,
+            pixel: 128,
         },
         {
             year: "2018",
             Iphone: 2000,
             Samsung: 9800,
+            pixel: 3334,
         },
         {
             year: "2019",
             Iphone: 2780,
             Samsung: 3908,
+            pixel: 5555,
+
         },
         {
             year: "2020",
             Iphone: 1890,
             Samsung: 4800,
+            pixel: 8512,
+
         },
         {
             year: "2021",
             Iphone: 2390,
             Samsung: 3800,
+            pixel: 1240,
+
         },
         {
             year: "2022",
             Iphone: 3490,
             Samsung: 4300,
+            pixel: 6540,
+
         },
     ];
 
@@ -95,6 +106,24 @@ const AreaChartPlot = () => {
                                 stopOpacity={0}
                             />
                         </linearGradient>
+                        <linearGradient
+                            id="colorPx"
+                            x1="0"
+                            y1="0"
+                            x2="0"
+                            y2="1"
+                        >
+                            <stop
+                                offset="5%"
+                                stopColor="#19d"
+                                stopOpacity={0.8}
+                            />
+                            <stop
+                                offset="95%"
+                                stopColor="#19d"
+                                stopOpacity={0}
+                            />
+                        </linearGradient>
                     </defs>
                     <XAxis dataKey="year" />
                     <YAxis />
@@ -113,6 +142,13 @@ const AreaChartPlot = () => {
                         stroke="#82ca9d"
                         fillOpacity={1}
                         fill="url(#colorPv)"
+                    />
+                    <Area
+                        type="monotone"
+                        dataKey="pixel"
+                        stroke="#19d"
+                        fillOpacity={1}
+                        fill="url(#colorPx)"
                     />
                 </AreaChart>
             </ResponsiveContainer>
