6a8c746ae24c0a5aea151caeb0534133ac99c341
patrice <patricedev02@gmail.com>
2023-12-04 12:55:10 +0000

    update area pie chart

--- app/charts/pieChart.jsx
+++ app/charts/pieChart.jsx
@@ -1,15 +1,22 @@
 "use client";
-import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
+import {
+    PieChart,
+    Pie,
+    Cell,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from "recharts";
 
 const PieChartPlot = () => {
-  const colors = [
-      "#8884d8",
-      "#FA8072",
-      "#AF69EE",
-      "#3DED97",
-      "#3AC7EB",
-      "#F9A603",
-  ];
+    const colors = [
+        "#8884d8",
+        "#FA8072",
+        "#AF69EE",
+        "#3DED97",
+        "#3AC7EB",
+        "#F9A603",
+    ];
     const data = [
         {
             name: "Twitter",
@@ -36,27 +43,28 @@ const PieChartPlot = () => {
             value: 18900,
         },
     ];
-  return (
-      <>
-          <ResponsiveContainer width="100%" height="100%">
-              <PieChart width={730} height={250}>
-                  <Pie
-                      data={data}
-                      dataKey="value"
-                      nameKey="name"
-                      cx="50%"
-                      cy="50%"
-                      fill="#8884d8"
-                      label
-                  >
-                      {data.map((entry, index) => (
-                          <Cell key={`cell-${index}`} fill={colors[index]} />
-                      ))}
-                  </Pie>
-                  <Tooltip />
-              </PieChart>
-          </ResponsiveContainer>
-      </>
-  );
+    return (
+        <>
+            <ResponsiveContainer width="100%" height="100%">
+                <PieChart width={730} height={250}>
+                    <Pie
+                        data={data}
+                        dataKey="value"
+                        nameKey="name"
+                        cx="50%"
+                        cy="50%"
+                        fill="#8884d8"
+                        label={data[0].name}
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[index]} />
+                        ))}
+                    </Pie>
+                    <Tooltip />
+                    <Legend />
+                </PieChart>
+            </ResponsiveContainer>
+        </>
+    );
 };
-export  {PieChartPlot}; 
+export { PieChartPlot };
