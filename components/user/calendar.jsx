bfad150c0b4c9e677ff798f09005b684319e6744
patrice <patricedev02@gmail.com>
2023-12-06 14:42:26 +0000

    update display text

--- components/user/calendar.jsx
+++ components/user/calendar.jsx
@@ -6,22 +6,13 @@ export const Calendar = ({ userData }) => {
             <div>
                 {userData.login && (
                     <div>
-                        <h2>GitHub Contributions Calendar</h2>
+                        <h2>Contribution Calendar</h2>
                         <GitHubCalendar
                             username={userData.login}
                             blockSize={15}
                             blockRadius={10}
                             blockMargin={2}
                             colorScheme="light"
-                            // renderBlock={(block, activity) => (
-
-                            //     <div
-                            //         className="tooltip"
-                            //         data-tip={`${activity.count} activities on ${activity.date}`}
-                            //     >
-                            //         {block}
-                            //     </div>
-                            // )}
                         />
                     </div>
                 )}
--- components/user/metrics.jsx
+++ components/user/metrics.jsx
@@ -1,6 +1,6 @@
 import { BarChartPlot } from "../charts/barChart";
-import { AreaChartPlot } from "../charts/areaChart";
-import { LineChartPlot } from "../charts/lineChart";
+// import { AreaChartPlot } from "../charts/areaChart";
+// import { LineChartPlot } from "../charts/lineChart";
 import { CommitsInRepository } from "../charts/commitsInRepository";
 import { ContributionsChart } from "../charts/contributionChart";
 import { ProgrammingLanguageChart } from "../charts/languageChart";
@@ -9,64 +9,86 @@ import { ContributionsChartForMonth } from "../charts/contributionsPerMonth";
 import { ContributionsChartForDay } from "../charts/contributionsPerDay";
 import { ContributionsChartForYear } from "../charts/contributionsPerYear";
 
-export function MetricsChart({ user }) {
+import style from "./_partials/_metrics.scss";
 
+export function MetricsChart({ user }) {
     return (
         <>
-            <section className="flex my-4 px-4 gap-3">
-                <div className=" w-1/2 h-[250px]  rounded">
-                    <h3 className="title">User contribuions</h3>
-                    <ContributionsChart user={user} />
-                </div>
-                <div className=" w-1/2 h-[250px]  rounded">
-                    <h3 className="title">{user.name} programing languages</h3>
+            <div className="container">
+                <section className="metrics">
+                    <div className="wrapper">
+                        <section className=" metrics--container">
+                            <div className="w-1/2 h-[300px]  rounded">
+                                <h3 className="title">
+                                    {user.name} commits vs contributor commits
+                                    in repository
+                                </h3>
+                                <BarChartPlot user={user} />
+                            </div>
+                            <div className=" w-1/2 h-[250px]  rounded">
+                                <h3 className="title">User contribuions</h3>
+                                <ContributionsChart user={user} />
+                            </div>
+                        </section>
+                        <section className=" metrics--container">
+                            <div className=" w-1/2 h-[250px]  rounded">
+                                <h3 className="title">
+                                    {user.name} programing languages
+                                </h3>
+                                <ProgrammingLanguageChart user={user} />
+                            </div>
+                            <div className="w-1/2 h-[300px]  rounded">
+                                <h3 className="title">
+                                    {user.name} commits vs contributor commits
+                                    in repository
+                                </h3>
+                                <BarChartPlot user={user} />
+                            </div>
+                        </section>
+                    </div>
+                </section>
 
-                    <ProgrammingLanguageChart user={user} />
-                </div>
-            </section>
-            <section className="flex my-4 px-4 gap-3">
-                <div className=" w-screen h-[250px]  rounded">
-                    <h3 className="title">Month contributions</h3>
+                <section className="metrics">
+                    <div className="wrapper">
+                        <section className="metrics--container">
+                            <div className=" w-screen h-[250px]  rounded">
+                                <h3 className="title">Month contributions</h3>
+                                <ContributionsChartForMonth user={user} />
+                            </div>
+                        </section>
 
-                    <ContributionsChartForMonth user={user} />
-                </div>
-            </section>
-            <section className="flex my-4 px-4 gap-3">
-                <div className="w-1/2 h-[300px]  rounded">
-                    <h3 className="title">
-                        {user.name} commits vs contributor commits in repository
-                    </h3>
-                    <BarChartPlot user={user} />
-                </div>
-                <div className="w-1/2 h-[300px]  rounded">
-                    <h3 className="title">All times contributions</h3>
-                    {/* <LineChartPlot user={user} /> */}
-                    <ContributionsChartForDay user={user} />
-                </div>
-            </section>
-            {/* <section className="flex my-4 px-4 gap-3">
-                <div className=" w-screen h-[250px]  rounded">
-                    <h3 className="title">{user.name}all times activities</h3>
-                    <AreaChartPlot user={user} />
-                </div>
-            </section> */}
-            <section className="flex my-4 px-4 gap-3">
-                <div className="w-1/2 h-[250px]  rounded">
-                    <h3 className="title">Commits per repository</h3>
-                    <CommitsInRepository user={user} />
-                </div>
-                <div className="w-1/2 h-[250px]  rounded">
-                    <h3 className="title">Year contribution</h3>
-                    <ContributionsChartForYear user={user} />
-                </div>
-            </section>
+                        <section className=" metrics--container">
+                            <div className="w-1/2 h-[250px]  rounded">
+                                <h3 className="title">
+                                    Commits per repository
+                                </h3>
+                                <CommitsInRepository user={user} />
+                            </div>
+                            <div className="w-1/2 h-[250px]  rounded">
+                                <h3 className="title">Year contribution</h3>
+                                <ContributionsChartForYear user={user} />
+                            </div>
+                        </section>
+                        <section className=" metrics--container">
+                            <div className="w-screen h-[300px]  rounded">
+                                <h3 className="title">
+                                    All times contributions
+                                </h3>
+                                <ContributionsChartForDay user={user} />
+                            </div>
+                        </section>
+                    </div>
+                </section>
 
-            <section className="flex my-4 px-4 gap-3">
-                <div className="w-screen h-[250px]  rounded">
-                    <h3 className="title">Repository contributions</h3>
-                    <RepositoryContributions user={user} />
-                </div>
-            </section>
+                <section className="metrics">
+                    <section className=" metrics--container">
+                        <div className="w-screen h-[250px]  rounded">
+                            <h3 className="title">Repository contributions</h3>
+                            <RepositoryContributions user={user} />
+                        </div>
+                    </section>
+                </section>
+            </div>
         </>
     );
 }
